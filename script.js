8651e16f54919305ddfe40577662b7204f0fab1e
Guy Bryant <guy.s.bryant@gmail.com>
2019-02-05 17:20:18 -0500

    Add nav div, nav ul, lis

---
+++ script.js
@@ -0,0 +1,6 @@
+window.onload = function(){
+    // navDiv = document.createElement('div');
+    // navUL = document.createElement()
+    // clickDiv.innerHTML = "<p>Sup</p>";
+    // document.body.appendChild(clickDiv);
+};
